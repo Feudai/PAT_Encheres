fae559bfbf64cf5d65da04286374cf039f65bd15
Feudai <tim.criaud@gmail.com>
2024-12-16 15:57:04 +0100

    Tim BO classes Getters/Setters

--- PAT_enchere/src/main/java/org/enchere/bo/Utilisateur.java
+++ PAT_enchere/src/main/java/org/enchere/bo/Utilisateur.java
@@ -43,4 +43,113 @@ public class Utilisateur {
 	public Utilisateur() {
 	
 }
+	
+	
+
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public float getCredit() {
+		return credit;
+	}
+
+	public void setCredit(float credit) {
+		this.credit = credit;
+	}
+
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+
+	public List<ArticleVendu> getArticlesAVendre() {
+		return articlesAVendre;
+	}
+
+	public void setArticlesAVendre(List<ArticleVendu> articlesAVendre) {
+		this.articlesAVendre = articlesAVendre;
+	}
+
+	public List<Enchere> getArticlesAEncherir() {
+		return articlesAEncherir;
+	}
+
+	public void setArticlesAEncherir(List<Enchere> articlesAEncherir) {
+		this.articlesAEncherir = articlesAEncherir;
+	}
+
+	public List<ArticleVendu> getArticleAAcheter() {
+		return articleAAcheter;
+	}
+
+	public void setArticleAAcheter(List<ArticleVendu> articleAAcheter) {
+		this.articleAAcheter = articleAAcheter;
+	}
+	
+	
+	
 }

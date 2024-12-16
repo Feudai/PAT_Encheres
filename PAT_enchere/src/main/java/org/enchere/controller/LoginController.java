2543ed4c6b97ca95f9b04f5f109f4044f6b2f023
BarkingBoy <paulperrot9@gmail.com>
2024-12-16 15:50:17 +0100

    paul - Création de la page accueil / entete / login

--- PAT_enchere/src/main/java/org/enchere/controller/LoginController.java
+++ PAT_enchere/src/main/java/org/enchere/controller/LoginController.java
@@ -2,17 +2,21 @@ package org.enchere.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
 public class LoginController {
 
 	
+	
 	@GetMapping("/connexion")
 	public String affichageConnexion () {
 		
 		return "connexion";
 	}
-	
+	@GetMapping("/accueil")
+	public String affichageAccueil() {
+		
+		return "accueil";
+	}
 }

b83a338568b9708972d3a6cef6f2d4744eb35a5a
Feudai <tim.criaud@gmail.com>
2024-12-17 16:07:02 +0100

    Tim est content

--- PAT_enchere/src/main/java/org/enchere/bll/EnchereService.java
+++ PAT_enchere/src/main/java/org/enchere/bll/EnchereService.java
@@ -1,5 +1,15 @@
 package org.enchere.bll;
 
+import java.util.List;
+
+import org.enchere.bo.Enchere;
+
 public interface EnchereService {
+
+	
+	public List<Enchere> ajouterEnchere(Enchere enchere);
+	
+	public List<Enchere> getListeEncheres();
+
 
 }
--- PAT_enchere/src/main/java/org/enchere/dal/EnchereDAO.java
+++ PAT_enchere/src/main/java/org/enchere/dal/EnchereDAO.java
@@ -1,5 +1,14 @@
 package org.enchere.dal;
 
+import java.util.List;
+
+import org.enchere.bo.Enchere;
+
 public interface EnchereDAO {
 
+public List<Enchere> create(Enchere enchere);
+
+		public List<Enchere> findAll();
+	
 }
+

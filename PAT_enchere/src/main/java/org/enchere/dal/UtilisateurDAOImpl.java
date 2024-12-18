2de74b1fe1aeec7a19822fca90edc6831aaca0d5
Feudai <tim.criaud@gmail.com>
2024-12-18 15:21:35 +0100

    tamere

--- PAT_enchere/src/main/java/org/enchere/dal/UtilisateurDAOImpl.java
+++ PAT_enchere/src/main/java/org/enchere/dal/UtilisateurDAOImpl.java
@@ -144,13 +144,7 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 	this.jdbcTemplate.update(UPDATE, map);
 	}
 
-<<<<<<< Updated upstream
-	@Override
-	public void deleteUser(int noUtilisateur) {
-		// TODO Auto-generated method stub
-		
-	}
-=======
+
 @Override
 public void deleteUser(int noUtilisateur) {
 	MapSqlParameterSource map = new MapSqlParameterSource();
@@ -159,7 +153,5 @@ public void deleteUser(int noUtilisateur) {
 	
 }
 
-
->>>>>>> Stashed changes
 
 }

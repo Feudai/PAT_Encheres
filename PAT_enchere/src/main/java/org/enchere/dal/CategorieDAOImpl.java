44c5c9d27de1d0dff559ca775ba047cba1a0a3f0
Feudai <tim.criaud@gmail.com>
2024-12-18 15:26:57 +0100

    tamere

--- PAT_enchere/src/main/java/org/enchere/dal/CategorieDAOImpl.java
+++ PAT_enchere/src/main/java/org/enchere/dal/CategorieDAOImpl.java
@@ -1,35 +1,39 @@
 package org.enchere.dal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.enchere.bo.Categorie;
-import org.enchere.bo.Enchere;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CategorieDAOImpl implements CategorieDAO {
 
+	
+	public List<Categorie> create(Categorie categorie){
+		
+		return null;
+	}
 
 		public List<Categorie> findAll(){
-			
-			return null;
-		}
+			//TEMPORAIRE
+			List<Categorie> exemple = new ArrayList<Categorie>();
+			exemple.add(new Categorie(1,"Informatique",null));
+			exemple.add(new Categorie(1,"Ameublement",null));
+			exemple.add(new Categorie(1,"Vêtements",null));
+			exemple.add(new Categorie(1,"Sport&Loisirs",null));
+			exemple.add(new Categorie(1,"ZiziLOL",null));
+			exemple.add(new Categorie(1,"PAUL-SENPAI",null));
+			exemple.add(new Categorie(1,"ANTOINE-DESU",null));
+			exemple.add(new Categorie(1,"KONO-TIMO",null));
 
-		@Override
-		public List<Categorie> create(Categorie categorie) {
-			// TODO Auto-generated method stub
-			return null;
+
+			return exemple;
 		}
 
 		@Override
 		public Categorie findById(int id) {
-			// TODO Auto-generated method stub
-			return null;
-		}
 
-		@Override
-		public Categorie findById(int id) {
-			// TODO Auto-generated method stub
 			return null;
 		}
 }
